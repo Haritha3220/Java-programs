24126b605f3241d81fd80b50a68d4008dcf4ac4f
Haritha3220 <challaharitha637@gmail.com>
2025-08-19 16:14:01 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class vote {
+ public static void main(String[] args) {
+ int letter = 65, i, j,rows;
+ System.out.println("Enter the number of rows : ");
+ Scanner in = new Scanner(System.in);
+ rows = in.nextInt();
+ for (i = 0; i <= rows; i++) {
+ for (j = 0; j <= i; j++) {
+ System.out.print((char)(letter + j)+" ");
+ }
+ System.out.println("");
+ }
