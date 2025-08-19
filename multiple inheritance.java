6b7d8ce87f5feb348bc16f3e1ba65d68d71a6e3c
Haritha3220 <challaharitha637@gmail.com>
2025-08-19 16:16:01 +0530

    Add files via upload

---
+++ multiple inheritance.java
@@ -0,0 +1,23 @@
+class GrandFather {
+public void house() { 
+System.out.println("3 BHK House.");
+}
+}
+class Father extends GrandFather { 
+public void land() {
+System.out.println("5 Arcs of Land.");
+}
+}
+class Son extends Father { 
+public void Car() {
+System.out.println("Own Audi car.");
+}
+}
+class multilevel {
+public static void main(String[] args) { 
+Son o = new Son();
+o.Car();
+o.house();
+o.land();
+}
+}
