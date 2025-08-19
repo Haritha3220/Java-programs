84942ceced7f74e865ba5b7d70de9359ad2f4e53
Haritha3220 <challaharitha637@gmail.com>
2025-08-19 16:12:06 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int a[]={10,15,20,19,22};
+		for(int i=0;i<a.length;i++){
+			if(a[i]>=18){
+				System.out.println(a[i]);
+      	}
+		}
+	}
+}
